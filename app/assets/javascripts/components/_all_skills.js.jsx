3b8041e40cee995def831f0de7e4099d93ec14a5
Emily Dowdle <emily@editingemily.com>
2016-02-22 12:57:59 -0700

    First commit: view, edit, delete

---
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -0,0 +1,31 @@
+var AllSkills = React.createClass({
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
+  handleEdit() {
+    this.props.handleEdit(id);
+  },
+
+  onUpdate(skill) {
+    this.props.onUpdate(skill);
+  },
+
+  render() {
+    var skills = this.props.skills.map((skill) => {
+      return (
+        <div key={skill.id}>
+          <Skill skill={skill}
+                 handleDelete={this.handleDelete.bind(this, skill.id)}
+                 handleUpdate={this.onUpdate}/>
+        </div>
+      )
+    });
+
+    return (
+      <div>
+        {skills}
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,62 @@
+var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeIdeaFromDOM(id);
+      }
+    });
+  },
+
+  removeIdeaFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState })
+  },
+
+  handleUpdate(skill) {
+    $.ajax({
+      url: `/api/v1/skills/${skill.id}`,
+      type: 'PUT',
+      data: { skill: skill },
+      success: () => {
+        console.log('you did it');
+        this.updateSkills(skill);
+      }
+    });
+  },
+
+  updateSkills(skill) {
+    var skills = this.state.skills.filter((s) => { return s.id != skill.id });
+    skills.push(skill);
+
+    this.setState({ skills: skills });
+  },
+
+  render() {
+    return (
+      <div>
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills}
+                   handleDelete={this.handleDelete}
+                   onUpdate={this.handleUpdate} />
+      </div>
+    )
+  }
+});
